f687e0160ba723b172be5e9a9c7046dcbb8c18e2
Patricia Silvestre Alvarenga <patriciasilvestrea@gmail.com>
2022-03-22 11:22:09 -0300

    requisito 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,18 @@
+# fonte de pesquisa:
+# https://www.geeksforgeeks.org/recursive-function-check-string-palindrome/
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if word == "":
+        return False
+    elif low_index == high_index:  # se só tem 1 caracter, será true
+        return True
+# se o primeiro e o último caracter não forem iguais, será false
+    elif word[low_index] != word[high_index]:
+        return False
+# se tem mais de 2 caracters, check se a string do meio
+# é tbm um palindrome
+    elif low_index < high_index + 1:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    else:
+        return True
+# Para ser recursiva:
+# condições de parada e a função faz chamada a si mesma
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,13 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    accumulator_in_target_time = 0
+    accumulator_students_in_target_time = 0
     try:
         for start_time, end_time in permanence_period:
             if start_time <= target_time <= end_time:
-                accumulator_in_target_time += 1
-        return accumulator_in_target_time
+                accumulator_students_in_target_time += 1
+        return accumulator_students_in_target_time
     except TypeError:
         return None
-# Para ir sendo "somado/acumulado" o target time,
-# tem que estar entre o start_time e o end time
+# Para ir sendo "somado/acumulado" o accumulator_students_in_target_time,
+# o target time, tem que estar entre o start_time e o end time.
 # Será retornado None, conforme solicitado no requisito,
-# se forem passados argumentos do tipo não apropriado
+# se forem passados argumentos do tipo não apropriado.
