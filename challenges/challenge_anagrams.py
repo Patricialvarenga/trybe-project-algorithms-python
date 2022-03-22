c46646b57c95f1d03661744b838d1e9e7b9dfa55
Patricia Silvestre Alvarenga <patriciasilvestrea@gmail.com>
2022-03-22 18:36:45 -0300

    requisito 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,13 @@
+from challenges.merge_sort import merge_sort
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if first_string == "" or second_string == "":
+        return False
+    if len(first_string) != len(second_string):
+        return False
+    if first_string != second_string:
+        return merge_sort(
+            first_string.lower()) == merge_sort(second_string.lower())
+    else:
+        return True
